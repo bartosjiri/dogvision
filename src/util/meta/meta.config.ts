f4a470929b13c9554681460a309ccc645e732a3a
bartosjiri <me@bartosjiri.com>
2023-12-02 12:13:33 +0100

    Add meta and analytics

---
+++ src/util/meta/meta.config.ts
@@ -0,0 +1,24 @@
+export const META_DOMAIN = 'https://dogvision.vercel.app';
+
+export const META_TITLE = `DogVision`;
+export const META_DESCRIPTION = "An app for visualizing a dog's color perception";
+export const META_KEYWORDS = ['dog', 'color', 'perception', 'simulation'];
+
+export const META_THEME_COLOR = '#ffffff';
+export const META_APPLE_COLOR = '#000000';
+export const META_MICROSOFT_COLOR = '#ffffff';
+
+export const META_OG_URL = META_DOMAIN;
+export const META_OG_IMAGE_URL = `${META_OG_URL}/meta/og-image.jpg`;
+export const META_OG_IMAGE_WIDTH = 1200;
+export const META_OG_IMAGE_HEIGHT = 630;
+
+export const META_VERSION = 'p16rgdtw5q';
+
+// <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+// <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+// <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+// <link rel="manifest" href="/site.webmanifest">
+// <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
+// <meta name="msapplication-TileColor" content="#ffffff">
+// <meta name="theme-color" content="#ffffff">
